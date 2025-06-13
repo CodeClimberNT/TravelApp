739e7d1f43d326e39291f272aff783fdabfd0698
Alessandro Dal Mas <Alessandrogiovannidalmas@gmail.com>
2025-06-13 11:09:01 +0200

    remove html codes

---
+++ app/src/main/java/com/example/final_assignment_even_g28/data_class/UserProfile.kt
@@ -0,0 +1,129 @@
+package com.example.final_assignment_even_g28.data_class
+
+import com.example.final_assignment_even_g28.ui.components.user_profile.IconType
+import com.example.final_assignment_even_g28.ui.components.user_profile.ProfilePictureData
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.Exclude
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserProfile(
+    var uid: String = "", // Firebase UID used for Google Authentication
+    var name: String = "",
+    var surname: String = "",
+    var fullName: String = "",
+    var nickName: String = "",
+    var typeOfExperiences: List<String> = emptyList(),
+    var mostDesiredDestination: String = "",
+    var phoneNumber: String = "",
+    val email: String = "",
+    @Contextual var dateOfBirth: Timestamp = Timestamp.Companion.now(),
+    var pastExperiences: List<String> = emptyList(),
+    var bio: String = "",
+    var badge: String = "",
+    var currentLevel: Int = 1,
+    var rating: Float = 0.0f,
+    var isProfileImage: String = "Monogram",
+    @get:Exclude
+    var profilePicture: ProfilePictureData = ProfilePictureData.Monogram(""),
+) {
+    constructor() : this(
+        uid = "",
+        fullName = "",
+        nickName = "",
+        typeOfExperiences = emptyList(),
+        mostDesiredDestination = "",
+        phoneNumber = "",
+        email = "",
+        pastExperiences = emptyList(),
+        bio = "",
+        badge = "",
+        currentLevel = 1,
+        rating = 0.0F,
+        profilePicture = ProfilePictureData.Monogram("")
+    )
+
+    constructor(nickName: String, profilePicture: ProfilePictureData) : this(
+        uid = "",
+        fullName = "",
+        nickName = nickName,
+        typeOfExperiences = emptyList(),
+        mostDesiredDestination = "",
+        phoneNumber = "",
+        email = "",
+        pastExperiences = emptyList(),
+        bio = "",
+        badge = "",
+        currentLevel = 1,
+        rating = 0.0F,
+        profilePicture = profilePicture
+    )
+
+    constructor(uid: String, nickName: String, profilePicture: ProfilePictureData) : this(
+        uid = uid,
+        fullName = "",
+        nickName = nickName,
+        typeOfExperiences = emptyList(),
+        mostDesiredDestination = "",
+        phoneNumber = "",
+        email = "",
+        pastExperiences = emptyList(),
+        bio = "",
+        badge = "",
+        currentLevel = 1,
+        rating = 0.0F,
+        profilePicture = profilePicture
+    )
+
+    constructor(fullName: String, rating: Float, description: String) : this(
+        uid = "",
+        fullName = fullName,
+        nickName = fullName,
+        typeOfExperiences = emptyList(),
+        mostDesiredDestination = "",
+        phoneNumber = "",
+        email = "",
+        pastExperiences = emptyList(),
+        bio = description,
+        badge = "",
+        currentLevel = 1,
+        rating = rating,
+        profilePicture = ProfilePictureData.Icon(IconType.ACCOUNT_CIRCLE)
+    )
+
+    constructor(name: String, rating: Float, avatar: ProfilePictureData, contact: String) : this(
+        uid = "4",
+//        uid = "13",
+        fullName = name,
+        nickName = name,
+        typeOfExperiences = emptyList(),
+        mostDesiredDestination = "Alice",
+        phoneNumber = "",
+        email = contact,
+        pastExperiences = emptyList(),
+        bio = "",
+        badge = "",
+        currentLevel = 1,
+        rating = rating,
+        profilePicture = avatar
+    )
+
+    constructor(uid: String, name: String, email: String) : this(
+        uid = uid,
+        fullName = name,
+        nickName = name,
+        typeOfExperiences = emptyList(),
+        mostDesiredDestination = "",
+        phoneNumber = "",
+        email = email,
+        pastExperiences = emptyList(),
+        bio = "",
+        badge = "",
+        currentLevel = 1,
+        rating = 0.0F,
+        profilePicture = ProfilePictureData.Icon(IconType.ACCOUNT_CIRCLE)
+    )
+}
+
+typealias Planner = UserProfile
