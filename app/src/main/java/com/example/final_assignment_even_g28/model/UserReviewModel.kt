8187d8ce86ca6d60274d3ace1ead3e6dc65b9df8
Nicolò Taormina <nicktaormina3@gmail.com>
2025-06-22 12:39:54 +0200

    Fixed review rating update

--- app/src/main/java/com/example/final_assignment_even_g28/model/UserReviewModel.kt
+++ app/src/main/java/com/example/final_assignment_even_g28/model/UserReviewModel.kt
@@ -2,6 +2,7 @@ package com.example.final_assignment_even_g28.model
 
 import android.util.Log
 import com.example.final_assignment_even_g28.data.Collections
+import com.example.final_assignment_even_g28.data_class.UserProfile
 import com.example.final_assignment_even_g28.data_class.UserReview
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
@@ -59,10 +60,52 @@ class UserReviewModel {
         return try {
             val docRef = Collections.userReview.add(review).await()
             Log.d("UserReviewModel", "Review written successfully: ${docRef.id}")
+            val userDocRef = Collections.users.document(review.reviewedUserUID).get().await()
+            if (userDocRef.exists()) {
+                userDocRef.toObject(UserProfile::class.java)?.let { user ->
+                    val reviews = getReviews(user.uid)
+                    val ratings = reviews.map { review ->
+                        review.rating
+                    }
+                    val averageRating = if (ratings.isNotEmpty()) {
+                        ratings.sum() / ratings.size
+                    } else {
+                        0f
+                    }
+                    if (averageRating < 0 || averageRating > 5) {
+                        throw IllegalArgumentException("Invalid average rating: $averageRating")
+                    }
+                    user.rating = averageRating
+                    Log.d("UserReviewModel", "User review count updated successfully")
+                    user.let { updatedUser ->
+                        Collections.users.document(updatedUser.uid).set(updatedUser)
+                            .await()
+                        Log.d("UserReviewModel", "User rating updated successfully")
+                        Result.success(docRef.id)
+                    }
+                }
+            } else {
+                throw IllegalArgumentException("User not found for UID: ${review.reviewedUserUID}")
+            }
             Result.success(docRef.id)
         } catch (e: Exception) {
-            Log.e("UserReviewModel", "Error writing review: ${e.message}")
+            Log.e("UserReviewModel", "${e.message}")
             Result.failure(e)
+        }
+    }
+
+    private suspend fun getReviews(userUID: String): List<UserReview> {
+        return try {
+            val snapshot =
+                Collections.userReview.whereEqualTo("reviewedUserUID", userUID).get().await()
+            val reviews = snapshot.documents.mapNotNull {
+                it.toObject(UserReview::class.java)
+            }
+            reviews
+
+        } catch (e: Exception) {
+            Log.e("UserReviewModel", "Error fetching total number of reviews: ${e.message}")
+            emptyList<UserReview>()
         }
     }
 }
